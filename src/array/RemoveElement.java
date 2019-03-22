0bcdb4930bd99a4ca24b7ed71f04e61d0c5d8cc3
dimi <1425920291@qq.com>
2019-03-22 16:46:16 +0800

    RemoveElement

--- src/array/RemoveElement.java
+++ src/array/RemoveElement.java
@@ -1,5 +1,7 @@
 package array;
 
+import java.util.Arrays;
+
 /**
  * Given an array nums and a value val, remove all instances of that value in-place and return the new length.
  * Given nums = [3,2,2,3], val = 3,Your function should return length = 2, with the first two elements of nums being 2.
@@ -13,29 +15,33 @@ public class RemoveElement {
 	/**
 	 * Runtime: 3 ms, faster than 100.00% of Java online submissions for Remove Element.
 	 */
-	public static int removeElement(int[] nums, int val) {
+	private static int removeElement(int[] nums, int val) {
 		int res = 0;
 		for (int i = 0; i < nums.length; i++) {
-			if(nums[i] != val){
-				nums[res++] = nums[i];
+			if(nums[i] == val){
+				nums[i] = 0;
 			}else {
-				nums[i]=0;
+				nums[res++] = nums[i];
 			}
 		}
 		return res;
 	}
 
 	public static void main(String[] args) {
-		int[] arr1 = new int[]{3,2,2,3};
 		int num1 = 3;
-		System.out.println(removeElement(arr1,num1));
+		int[] arr1 = new int[]{3,2,2,3};
+		int[] expect1 = new int[]{2,2};
+		int res1 = removeElement(arr1,num1);
+		boolean flag1 = Arrays.equals(Arrays.stream(arr1).limit(res1).toArray(),expect1);
+		System.out.println("falg1:" + flag1);
 
-		int[] arr2 = new int[]{0,1,2,2,3,0,4,2};
+
 		int num2 = 2;
-		System.out.println(" arr2 test--->"+removeElement(arr2,num2));
-		for (int i = 0; i <arr2.length; i++) {
-			System.out.println(arr2[i]);
-		}
+		int[] arr2 = new int[]{0,1,2,2,3,0,4,2};
+		int[] expect2 = new int[]{0, 1, 3, 0, 4};
+		int res2 = removeElement(arr2,num2);
+		boolean flag2 = Arrays.equals(Arrays.stream(arr2).limit(res2).toArray(),expect2);
+		System.out.println("flag2:" + flag2);
 	}
 
 }
