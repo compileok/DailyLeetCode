f1631d03f1b5de9c8e24c885dde67e4a30682e57
github <love.xudeming@gmail.com>
2019-05-17 21:43:46 +0800

    lingkedlist delete  binarysearch

---
+++ src/array/BinarySearch.java
@@ -0,0 +1,39 @@
+package array;
+
+public class BinarySearch {
+
+
+    public static int execute(int[] array, int target){
+        if(array == null) {
+            return -1;
+        }
+
+        int start = 0;
+        int end = array.length - 1;
+
+        int middle;
+        while (start <= end) {
+            middle = (start + end) / 2;
+
+            if(target == array[middle]) {
+                return middle;
+            }else if(target > array[middle]){
+                start = middle + 1;
+            }else {
+                end = middle -1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,3,4,5,7,8,9,10,12,13,15,17};
+        System.out.println( 8 == execute(arr,12));
+        System.out.println(2 == execute(arr,4));
+        System.out.println(-1 == execute(arr,2));
+
+
+    }
+
+
+}
---
+++ src/linkedlist/DeleteNodeInALinkedList.java
@@ -0,0 +1,14 @@
+package linkedlist;
+
+public class DeleteNodeInALinkedList {
+
+
+    public void deleteNode(ListNode node) {
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
